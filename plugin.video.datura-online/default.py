8b9ade778833233e1b60f0bc95ec628ddf514a28
yulivee <coding@voiding-warranties.de>
2017-02-18 20:01:47 +0100

    Login works, toying with structure

--- plugin.video.datura-online/default.py
+++ plugin.video.datura-online/default.py
@@ -1,11 +1,53 @@
+import xbmc, xbmcaddon
 import weblogin
 import yaml
+import CommonFunctions
+
+common = CommonFunctions
+logged_in = False
 
 
-with open("resources/data/credentials.yml", 'r') as yamlfile:
-    cfg = yaml.load(yamlfile)
+#with open("resources/data/credentials.yml", 'r') as yamlfile:
+#    cfg = yaml.load(yamlfile)
+#
+#logged_in = weblogin.doLogin('resources/data/',cfg['username'],cfg['password'])
+#
+#if logged_in == True:
+#    print "Login Successful"
 
-logged_in = weblogin.doLogin('resources/data/',cfg['username'],cfg['password'])
+def get_credentials ():
+    with open("resources/data/credentials.yml", 'r') as yamlfile:
+        cfg = yaml.load(yamlfile)
 
-if logged_in == True:
-    print "Login Successful"
+    return cfg['username'], cfg['password']
+
+def Notify ( title, message, times, icon ) :
+    xbmc.executebuiltin("XBMC.Notification("+title+","+message+","+times+","+icon+")")
+
+def Login ( username, password, hidesuccess ) :
+    uc = username[0].upper() + username[1:]
+    lc = username.lower
+
+    logged_in = weblogin.doLogin(__datapath__,username,password)
+    if logged_in == True:
+        avatar = get_avatar(lc)
+
+def addDir(name,url,mode,iconimage):
+    u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
+    ok=True
+    liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+    liz.setInfo( type="Video", infoLabels={ "Title": name } )
+    ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
+    return ok
+def addLink(name,url,iconimage):
+    ok=True
+    liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
+    liz.setInfo( type="Video", infoLabels={ "Title": name } )
+    ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
+    return ok
+
+ main_menu () :
+    if logged_in == False:
+        addDir("Sign In")
+
+
